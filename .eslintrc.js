eaddadfb03c58c4b7961486b093bc21f8254c578
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2019-11-17 00:55:20 -0300

    Adicionado recurso de Rotas e StyledComponents.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,6 @@ module.exports = {
   },
   extends: [
     'airbnb',
-    'prettier',
-    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
@@ -22,11 +20,29 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
   ],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { 'extensions': ['.js', '.jsx'] }],
-    'import/prefer-default-export': 'off'
+    'react/jsx-curly-spacing': [2, { 'when': 'always', 'allowMultiline': true }],
+    'react/prop-types': 'off',
+    'arrow-body-style': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'no-underscore-dangle': ['error', { 'allow': ['_id'] }],
+    // 'semi': ['error', 'never'],
+    'no-console': ['error', { 'allow': ['log', 'error', 'tron'] }],
+    'template-curly-spacing': ['error', 'always'],
+    'no-use-before-define': ['error', { 'variables': false }],
+    'react/jsx-one-expression-per-line': 'off',
+    'no-shadow': 'off',
+    'react/jsx-filename-extension': ['error', { 'extensions': ['.js', '.jsx'] }],
+    'global-require': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-expressions': ['error', { 'allowTaggedTemplates': true }],
+    'import/no-named-as-default': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePatter': '^_' }],
+    'consistent-return': 'off',
+    'no-useless-return': 'off',
+    'no-debugger': 'off',
+    'react/state-in-constructor': 'off',
+    'max-len': ['error', { 'code': 120 }]
   },
 };
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main/index';
+import Repository from './pages/Repository/index';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Main } />
+        <Route path="/repository" component={ Repository } />
+      </Switch>
+    </BrowserRouter>
+  );
+}
